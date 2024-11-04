49c94e4cab6001a93e65288ea82b6939a21a65e8
Wojciech Snopek <wojciech.snopek@useindio.com>
2024-11-04 14:22:12 +0100

    Initial project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+export const App = () => {
+
+  return (
+    <div className='container'>
+      <div className='title'>
+        <div>PETTER</div>
+        <div className='subtitle'>
+          Let us take care of your pets
+        </div>
+      </div>
+    </div>
+  )
+}
